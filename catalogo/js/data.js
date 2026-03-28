08ba6821ef4a07be9490a7eb8303b2c2514930c6
Lucas3255 <lucashenrique102006@gmail.com>
2026-03-28 02:14:35 -0300

    modificando icons js

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -1,6 +1,6 @@
 export const categories = [
     {
-        title: "Épicos",
+        title: "Filmes",
         items: [
             {
                 img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
@@ -21,21 +21,28 @@ export const categories = [
         ]
     },
     {
-        title: "Séries",
+        title: "Continue Assistindo",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/36edcf0527cc1cafabecf5e27647b73e10c96cca-1920x1080.png?accountingTag=ARCANE", top10: true, badge: "Nova temporada", badgeColor: "red", progress:90,
+                youtube: "https://www.youtube.com/watch?v=OkscEokV238&pp=ygUGYXJjYW5l" 
+            },
+
+            { img: "https://rollingstone.com.br/wp-content/uploads/brooklyn_99_div_fox.jpg", top10: true, progress:25,
+                youtube: "https://www.youtube.com/watch?v=icTOP9F17pU&pp=ygULYjk5dHJhaWxlciA%3D" 
+            },
+
+            { img: "https://i.ytimg.com/vi/eGV7zwjvxKs/maxresdefault.jpg", top10: true, progress:75,
+                youtube: "https://www.youtube.com/watch?v=dSDpoobO6yM&pp=ygUPZm5hZiAyIHRyYWlsZXIg" 
+            },
         ]
     },
     {
-        title: "Para maratonar",
+        title: "Animes",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://www.animeunited.com.br/oomtumtu/2025/08/306-dandadan-anime-poster-momo-dandadan-okarun-dandadan.png", top10: true, youtube: "https://youtu.be/oy-XD_gGbcE" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/a72dcec6d4fb54bb50c1f4880757c6df1c6258036e014fe03480fd0f127ad68d.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=LV-nazLVmgo&pp=ygUXYXRhY2sgb24gdGl0YW4gdHJhaWxlciA%3D" },
+            { img: "https://a.storyblok.com/f/178900/1127x634/c435e0a178/f91275e6e5a3c15be90358c6ead433ca1658949658_main.png/m/filters:quality(95)format(webp)", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=l96zmDlWCBk&pp=ygUVY2hhaW5zYXcgbWFuIHRyYWlsZXIg" },
+            { img: "https://occ-0-8407-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABXAsbrFrRnxme2plX5qzC7FVSX8dPahvg-ZHnF8CU8m4rqtVg7fV2iPuoagCalvXh82DGn8-MhZew6vKR3VptGIRgvrvgpMIvxwr.jpg?r=df6", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=erF7lSzkTYA&pp=ygUNa29taSB0cmFpbGVyIA%3D%3D" },
         ]
     }
 ];
